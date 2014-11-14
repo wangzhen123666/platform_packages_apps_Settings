39537e51241f21dddc38a9bddd2d8c06fbc5a7cb
Siavash Arya <siavash.arya@sonymobile.com>
2014-11-14 14:04:30 +0100

    Show more accurate internal storage capacity
    Show real total space instead of total user space
    and add item in storage to show size of the Android System.
    
    Bug: 28327846
    
    Change-Id: If177aa1ac63bcb584c3e50f6142976cabd390c94
    
    Conflicts:
    	src/com/android/settings/Utils.java

--- src/com/android/settings/Utils.java
+++ src/com/android/settings/Utils.java
@@ -54,6 +54,7 @@ import android.net.LinkProperties;
 import android.net.Uri;
 import android.os.BatteryManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.IBinder;
 import android.os.INetworkManagementService;
 import android.os.RemoteException;
@@ -100,6 +101,7 @@ import com.android.settingslib.drawable.CircleFramedDrawable;
 import com.android.settings.bluetooth.BluetoothSettings;
 import com.android.settings.wifi.SavedAccessPointsWifiSettings;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.InetAddress;
@@ -108,6 +110,8 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 import static android.content.Intent.EXTRA_USER;
 
@@ -160,6 +164,8 @@ public final class Utils {
     private static final int SECONDS_PER_DAY = 24 * 60 * 60;
 
     public static final String OS_PKG = "os";
+
+    private static final long GB_IN_BYTES = 1024 * 1024 * 1024;
 
     private static SparseArray<Bitmap> sDarkDefaultUserBitmapCache = new SparseArray<Bitmap>();
 
@@ -1334,6 +1340,29 @@ public final class Utils {
             default:
                 return res.getString(R.string.radioInfo_unknown);
         }
+
+    public static long getSystemTotalSpace() {
+        File system = Environment.getRootDirectory();
+        return system.getTotalSpace();
+    }
+
+    public static long estimateTotalSpace(Context context, long approximateTotalSpace) {
+        int[] possibleSizeBases = context.getResources()
+                .getIntArray(R.array.config_storageSizes);
+
+        SortedSet<Long> possibleSizes = new TreeSet<Long>();
+        for (int possibleSizeBase : possibleSizeBases) {
+            possibleSizes.add(possibleSizeBase * GB_IN_BYTES);
+        }
+
+        long estimatedTotal = approximateTotalSpace;
+        for (long possibleSize : possibleSizes) {
+            if (possibleSize > approximateTotalSpace) {
+                estimatedTotal = possibleSize;
+                break;
+            }
+        }
+        return estimatedTotal;
     }
 
     public static boolean isPackageInstalled(Context context, String pkg, boolean ignoreState) {
