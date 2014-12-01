e85e3f9b98851e86ed33fe37e8dbbb637be59418
Danesh M <daneshm90@gmail.com>
2014-12-01 09:29:57 -0600

    Settings: Add proximity wake option
    Ports and combines:
    
    http://review.cyanogenmod.org/#/c/66658/
    Settings : Add preference for proximity wake
    
    http://review.cyanogenmod.org/#/c/69347/
    Proximity Wake-Up : adjust strings
    
    http://review.cyanogenmod.org/#/c/69803/
    Settings : Make proximity opt-in
    
    Change-Id: Iea0853eccf764438c7aaac76afd13c364fcea0ec
    
    Conflicts:
    	res/values/cm_strings.xml
    	res/xml/display_settings.xml
    	src/com/android/settings/DisplaySettings.java
    
    Conflicts:
    	res/values/custom_strings.xml
    	res/xml/display_settings.xml
    	src/com/android/settings/DisplaySettings.java
    
    Conflicts:
    	res/values/custom_strings.xml
    
    Conflicts:
    	res/values/custom_strings.xml
    	res/xml/display_settings.xml
    	src/com/android/settings/DisplaySettings.java

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -88,6 +88,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private static final String KEY_WAKEUP_WHEN_PLUGGED_UNPLUGGED = "wakeup_when_plugged_unplugged";
     private static final String KEY_WAKEUP_CATEGORY = "category_wakeup_options";
     private static final String KEY_VOLUME_WAKE = "pref_volume_wake";
+    private static final String KEY_PROXIMITY_WAKE = "proximity_on_wake";
 
     private static final int DLG_GLOBAL_CHANGE_WARNING = 1;
 
@@ -244,8 +245,11 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
             mWakeUpWhenPluggedOrUnplugged.setOnPreferenceChangeListener(this);
         }
 
-        if (counter == 2) {
-            prefSet.removePreference(mWakeUpOptions);
+        boolean proximityCheckOnWait = getResources().getBoolean(
+                com.android.internal.R.bool.config_proximityCheckOnWake);
+        if (!proximityCheckOnWait) {
+            mWakeUpOptions.removePreference(findPreference(KEY_PROXIMITY_WAKE));
+            Settings.System.putInt(getContentResolver(), Settings.System.PROXIMITY_ON_WAKE, 1);
         }
     }
 
