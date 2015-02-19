2def18a776f528998acb8ec550838d8ee755ba92
snak3ater <saeedyasir91@gmail.com>
2015-02-19 14:51:13 +0500

    ZephyrOS Settings bringup
    - Add Zephyr dashboard tile
    - Add lock screen and status bar setting fragments
    - Disable lock screen settings for now
    - Using Viewpager layout
    - Viewpager layout improvments
    - Use PagerSlidingTabStrip for the tabs
    - Switch the sliding tabs to material design like tabs
    - Cleanup PagerSlidingTabStrip
    - Use material_deep_teal_500 for indicator color
    
    https://github.com/SAOSP-L/platform_packages_apps_Settings/commit/74513dd8aa18d1094205aa0df2b3b1129c875738
    https://github.com/SAOSP-L/platform_packages_apps_Settings/commit/3d813fb2372cda56d2742b06d6a697ec6c142ae8
    https://github.com/SAOSP-L/platform_packages_apps_Settings/commit/925c68952329721e756997aa2419f358194adca3
    https://github.com/SAOSP-L/platform_packages_apps_Settings/commit/a69a49c94461e20648a809c1401d94f9659b8b63
    https://github.com/SAOSP-L/platform_packages_apps_Settings/commit/925c68952329721e756997aa2419f358194adca3
    Signed-off-by: StarkDroid <gamerprince.exp@gmail.com>
    
    Conflicts:
    	res/values/strings.xml

---
+++ src/com/android/settings/simpleaosp/LockScreenSettings.java
@@ -0,0 +1,44 @@
+package com.android.settings.simpleaosp;
+
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.SwitchPreference;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+import com.android.internal.logging.MetricsLogger;
+
+public class LockScreenSettings extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+		// private variables here
+		
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.lock_screen_settings);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+
+		// preference changes here
+        return false;
+    }
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+ 		return super.onPreferenceTreeClick(preferenceScreen, preference);
+    	}
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsLogger.APPLICATION;
+    }
+}
+
