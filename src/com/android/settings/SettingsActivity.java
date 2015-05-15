4b633d784d1dc2a010b2d1f0776b3c6c8ad047cb
snak3ater <saeedyasir91@gmail.com>
2015-05-16 00:25:56 +0500

    QS: Longclick action [2/2]
    * Long press expanded status bar Settings button opens tiles settings
    * Fix Issue: Longpress opening settings app and QS tiles settings both.
    
    Conflicts:
    	AndroidManifest.xml
    	src/com/android/settings/Settings.java
    	src/com/android/settings/SettingsActivity.java
    
    Change-Id: Ic1d705346f04bee71906fb2f3a15dfa6b5122733

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -80,6 +80,7 @@ import com.android.settings.applications.UsageAccessDetails;
 import com.android.settings.applications.WriteSettingsDetails;
 import com.android.settings.bluetooth.BluetoothSettings;
 import com.android.settings.zephyr.DisplayRotation;
+import com.android.settings.cyanogenmod.qs.QSTiles;
 import com.android.settings.dashboard.DashboardCategory;
 import com.android.settings.dashboard.DashboardSummary;
 import com.android.settings.dashboard.DashboardTile;
@@ -356,6 +357,7 @@ public class SettingsActivity extends Activity
             DrawOverlayDetails.class.getName(),
             WriteSettingsDetails.class.getName(),
             DisplayRotation.class.getName()
+ 	    QSTiles.class.getName()
     };
 
 
